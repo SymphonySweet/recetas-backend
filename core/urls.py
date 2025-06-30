2f667b27fb47260e6fa46945bd54cbf3af727744
Lucho <luchiconde71@gmail.com>
2025-06-30 14:56:42 -0500

    Corrijo error de sintaxis en urls

--- core/urls.py
+++ core/urls.py
@@ -1,6 +1,11 @@
 from django.contrib import admin
 from django.urls import path
+from django.http import HttpResponse
+
+def home(request):
+    return HttpResponse("¡Hola desde Render y Django somos el grupo 2 RECETAS DE COCINA Y MAS!")
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', home),  # ← esta línea muestra algo en la raíz "/"
 ]

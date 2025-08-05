24a8dacef830603a049366db59173a0fac13afd8
Lucho <luchiconde71@gmail.com>
2025-08-05 13:29:10 -0500

    Agrego endpoint GET /recipes para listar recetas

--- routes/recipes.py
+++ routes/recipes.py
@@ -1,10 +1,21 @@
 from fastapi import APIRouter
 from models.recipe import Recipe
 from database import db
+from bson import ObjectId
 
 router = APIRouter(prefix="/recipes", tags=["Recipes"])
 
+# Crear receta
 @router.post("/")
 def create_recipe(recipe: Recipe):
     db.recipes.insert_one(recipe.dict())
     return {"message": "Receta agregada"}
+
+# Obtener todas las recetas
+@router.get("/")
+def get_recipes():
+    recetas = []
+    for receta in db.recipes.find():
+        receta["_id"] = str(receta["_id"])  # Convertir ObjectId a string
+        recetas.append(receta)
+    return recetas
